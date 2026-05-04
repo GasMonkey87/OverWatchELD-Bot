523dddf326373c6a12ab677eb4eb165181b4ee25
GasMonkey87 <2kvsgms7tk@privaterelay.appleid.com>
2026-05-04 18:36:51 -0400

    Add files via upload

--- Models/Requests.cs
+++ Models/Requests.cs
@@ -32,6 +32,11 @@ public sealed class SendMessageReq
     public string? AssignedTruckId { get; set; }
 
     public string Text { get; set; } = "";
+    public string? Body { get; set; }
+    public string? Message { get; set; }
+    public string? Content { get; set; }
+    public string? Route { get; set; }
+    public string? Direction { get; set; }
     public string? Source { get; set; }
 }
 
